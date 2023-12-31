051da166333e86b4dc379eb567e4d0be3fd4a956
Ren911308 <144767954+Ren911308@users.noreply.github.com>
2023-12-31 04:39:01 +0000

    修改ansic

---
+++ images/1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()  
+{
+    // Print Name
+    printf("Name   : 林祐任\n"); 
+
+    // Print Date of Birth
+    printf("DOB    :  14, 2004\n"); 
+
+    // Print Mobile Number
+    printf("Mobile : 0931-819089\n"); 
+
+    // Indicate successful execution
+    return(0); 
+}
